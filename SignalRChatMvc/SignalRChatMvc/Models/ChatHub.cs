c10bb19ff8522cc1eea922be6c63e10ebf207143
Alexander <asilva943rj@gmail.com>
2019-02-05 18:34:47 -0200

    First commit of the project.

---
+++ SignalRChatMvc/SignalRChatMvc/Models/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatMvc.Models
+{
+    public class ChatHub:Hub
+    {
+        public void Send(string nickname, string message)
+        {
+            Clients.All.SendAsync(nickname, message);
+        }
+    }
+}
